6287c577edce101ae8cf6055a1cd68e45195f7fe
hokyun <hgk9872@gmail.com>
2022-01-04 00:40:26 +0900

    Implement method

--- src/hgk/calendar/Calendar.java
+++ src/hgk/calendar/Calendar.java
@@ -3,13 +3,13 @@ package hgk.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-------------");
@@ -18,15 +18,15 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 78");
 	}
-	
+
 	public static void main(String[] args) {
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		//scanner 커서 올리고 ctrl + shift + m
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		// scanner 커서 올리고 ctrl + shift + m
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
-		
+
 		System.out.println(month + "월은 " + cal.getMaxDaysOfMonth(month) + "일");
 		scanner.close();
 	}
